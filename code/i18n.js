19669951dc033ad012db0063b1cd9a665b15f4ee
Diego González Fernández <verogeid@gmail.com>
2025-12-10 13:29:32 +0100

    fix sincronizacion. update currentindex to use app.STATE.lastDetailFocusIndex

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.0.88',
+        'version': '1.0.89',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
