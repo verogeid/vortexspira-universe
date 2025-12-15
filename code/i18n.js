226cfd54c632b3fa983ba7bcca0e99c827d44904
Diego González Fernández <verogeid@gmail.com>
2025-12-15 12:57:35 +0100

    style details text

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.1.14',
+        'version': '1.1.15',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
