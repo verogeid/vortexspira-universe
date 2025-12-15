70559e3c9bdec676e339ab4ecb50d1717072cf31
Diego González Fernández <verogeid@gmail.com>
2025-12-15 12:52:35 +0100

    fix favicon path

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.1.13',
+        'version': '1.1.14',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
