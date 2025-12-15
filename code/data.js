c9b04255c629a35c9ea0ac556d4033102acfb090
Diego González Fernández <162438292+verogeid@users.noreply.github.com>
2025-12-15 14:02:56 +0100

    Actualizar data.js

--- code/data.js
+++ code/data.js
@@ -6,7 +6,7 @@ export const LOGO_CARPETA = '📂';
 export const LOGO_CURSO = '📚';
 export const LOGO_VOLVER = '↩';
 export const LOGO_BUY = '🛒';
-export const LOGO_DISABLED = '🔒​';
+export const LOGO_DISABLED = '🔒';
 export const LOGO_A11Y = '🌐';
 
 export const PRIMARY_COLOR = '#999';
