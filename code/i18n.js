514bbddb6d2abe4069e3a1d7daa70058eb04bfdd
Diego González Fernández <verogeid@gmail.com>
2025-12-16 11:42:51 +0100

    fix 1024 issue

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.1.32',
+        'version': '1.1.33',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
