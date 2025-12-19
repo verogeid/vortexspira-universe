1014351fd360d1f84d84f561ca82a1d4ecf35fad
Diego González Fernández <162438292+verogeid@users.noreply.github.com>
2025-12-19 14:21:28 +0100

    Actualizar i18n.js
    prevent 2 fingers to move swipe

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.1.46',
+        'version': '1.1.47',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
