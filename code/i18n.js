be055b3a2b5c9ad9656dd09be1327bcec38d451c
Diego González Fernández <162438292+verogeid@users.noreply.github.com>
2025-12-19 16:45:04 +0100

    Actualizar i18n.js
    added a fill card at the end od mobile mode

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.1.48',
+        'version': '1.1.49',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
