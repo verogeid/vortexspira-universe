6a95c3e60caa237772d6d41e49f0899bcbb313b4
Diego González Fernández <162438292+verogeid@users.noreply.github.com>
2025-12-20 11:29:15 +0100

    Actualizar i18n.js

--- code/i18n.js
+++ code/i18n.js
@@ -5,7 +5,7 @@ import * as data from './data.js';
 
 export const STRINGS = {
     'es': {
-        'version': '1.1.66',
+        'version': '1.1.67',
 
         // Meta y Títulos
         'pageTitle': 'VortexSpira® Universe: Selector de Cursos',
